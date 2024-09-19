537bb302bd21c8ba8deb222f0eb90e88b3f8b522
Polymerase3 <polimerazaiii@gmail.com>
2024-09-19 08:22:00 -0400

    .get_formula_vars is ready to use

--- R/utils.R
+++ R/utils.R
@@ -4,6 +4,9 @@
 .get_formula_vars <- function(formula, data = NULL, ...) {
   args <- list(...)
   parent_env <- environment(formula)
+  eval.model.matrx <- !(any(c("|", "||") %in% all.names(formula)))
+  print(all.names(formula))
+  print(eval.model.matrx)
 
   ## Check data; if not exists the look for the data in the parent env----------
   if(!is.null(data)) {
@@ -43,14 +46,14 @@
 
   covs <- list()
   lapply(seq_along(rhs_vars), function(i) {
-    tryCatch(covs[[i]] <<- eval(rhs_vars[[i]], data, parent_env),
+    tryCatch(test <- eval(rhs_vars[[i]], data, parent_env), #covs[[i]] <<-
              error = function(e) {
                chk::abort_chk('All variables in the `formula` must be columns
                               in the `data` or objects in the global environment.')
              })
   })
 
-  covs <- setNames(covs, rhs_vars_char)
+  #covs <- setNames(covs, rhs_vars_char)
 
   ## dealing with interactions--------------------------------------------------
   rhs_labels <- attr(vars_covs, 'term.labels')
@@ -97,12 +100,81 @@
   if(is.null(rhs_labels)) {
     new_form <- as.formula('~0')
     vars_covs <- terms(new_form)
+    covs <- data.frame(Intercept = rep.int(1, if (is.null(treat)) 1L else
+      length(treat)))[, -1, drop = FALSE]
+
+  } else {
+    new_form_char <- sprintf("~ %s", paste(vapply(names(rhs_labels_list), function(x) {
+      if (x %in% rhs_vars_char[rhs_if_df]) paste0("`", rhs_labels_list[[x]], "`", collapse = " + ")
+      else rhs_labels_list[[x]]},
+      character(1L)), collapse = " + "))
+
+    new_form <- as.formula(new_form_char)
+    vars_covs <- terms(update(new_form,  ~ . - 1))
+
+    # Get model.frame
+    mf_covs <- quote(stats::model.frame(vars_covs, data,
+                                        drop.unused.levels = TRUE,
+                                        na.action = 'na.pass'))
+
+    tryCatch({covs <- eval(mf_covs)},
+             error = function(e) {
+               chk::abort_chk(conditionMessage(e), tidy = TRUE)
+             })
+
+    if(!is.null(treat_name) && treat_name %in% names(covs)) {
+      chk::abort_chk('The treatment variable cannot appear on the right side
+                     of the formula')
+    }
   }
 
+  if(eval.model.matrx) {
+    original_covs_levels <- .make_list(names(covs))
+
+    for (i in names(covs)) {
+      if (is.character(covs[[i]]))
+        covs[[i]] <- factor(covs[[i]])
+      else if (!is.factor(covs[[i]]))
+        next
+
+      if (length(unique(covs[[i]])) == 1L) {
+        covs[[i]] <- 1
+      }
+      else {
+        original_covs_levels[[i]] <- levels(covs[[i]])
+        levels(covs[[i]]) <- paste0("", original_covs_levels[[i]])
+      }
+    }
+
+    #Get full model matrix with interactions too
+    covs_matrix <- model.matrix(vars_covs, data = covs,
+                                contrasts.arg = lapply(Filter(is.factor, covs),
+                                                       contrasts, contrasts = FALSE))
+
+    for (i in names(covs)[vapply(covs, is.factor, logical(1L))]) {
+      levels(covs[[i]]) <- original_covs_levels[[i]]
+    }
+
+  } else {
+    covs_matrix <- NULL
+  }
 
   # Defining the list to return-------------------------------------------------
   ret <- list(treat = treat_data,
               treat_name = treat_name,
-              covs = covs)
-  #return(ret)
+              reported_covs = covs,
+              model_covs = covs_matrix)
+  return(ret)
+}
+
+
+#R Processing
+.make_list <- function(n) {
+  if (length(n) == 1L && is.numeric(n)) {
+    vector("list", as.integer(n))
+  }
+  else if (length(n) > 0L && is.atomic(n)) {
+    setNames(vector("list", length(n)), as.character(n))
+  }
+  else stop("'n' must be an integer(ish) scalar or an atomic variable.")
 }
